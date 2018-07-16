dbb321f8d1bd9cd55390fe272cd8b7c2281ba630
fsmflying <fangmingdev@163.com>
2018-07-16 19:39:41 +0800

    mysql数据库初始化

---
+++ mysql/init/init.sql
@@ -0,0 +1,6 @@
+use mysql;
+GRANT ALL ON *.* TO 'root'@'%';
+flush privileges;
+ALTER USER 'root'@'localhost' IDENTIFIED BY 'fangming' PASSWORD EXPIRE NEVER;
+ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'fangming';
+flush privileges;
