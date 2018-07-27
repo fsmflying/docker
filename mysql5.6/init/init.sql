786d6ce4d539653f33599597233983d4cc5f9a95
fsmflying <fangmingdev@163.com>
2018-07-27 09:59:00 +0800

    修改mysql5.6的init.sql

--- mysql5.6/init/init.sql
+++ mysql5.6/init/init.sql
@@ -1,6 +1,4 @@
 use mysql;
 GRANT ALL ON *.* TO 'root'@'%';
-flush privileges;
-ALTER USER 'root'@'localhost' IDENTIFIED BY 'fangming' PASSWORD EXPIRE NEVER;
 ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'fangming';
 flush privileges;
