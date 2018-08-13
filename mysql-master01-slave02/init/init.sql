2a373e741690ca74498dd603232b8eef4dce4057
fsmflying <fangmingdev@163.com>
2018-08-14 01:48:09 +0800

    添加mysql一主两从部署方案

--- mysql-master01-slave02/init/init.sql
+++ mysql-master01-slave02/init/init.sql
@@ -2,3 +2,4 @@ use mysql;
 ALTER USER 'root'@'%' IDENTIFIED BY 'fangming' WITH mysql_native_password BY 'fangming' PASSWORD EXPIRE NEVER;
 GRANT ALL ON *.* TO 'root'@'%';
 FLUSH PRIVILEGES;
+
--- mysql-master01/init/init.sql
+++ mysql-master01/init/init.sql
@@ -1,6 +1,12 @@
-use mysql;
+#mysql 8.0.11
+USE mysql;
+#设置密码以及访问来源
+ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'fangming' PASSWORD EXPIRE NEVER;
 GRANT ALL ON *.* TO 'root'@'%';
-flush privileges;
-ALTER USER 'root'@'localhost' IDENTIFIED BY 'fangming' PASSWORD EXPIRE NEVER;
-ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'fangming';
-flush privileges;
+FLUSH PRIVILEGES;
+
+#创建同步账号
+CREATE USER 'backup'@'%' IDENTIFIED BY 'abc123def';
+GRANT REPLICATION SLAVE ON *.* TO 'backup'@'%';
+FLUSH PRIVILEGES;
+
