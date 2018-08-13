76bb640e82b3788e19ee3df4c3ccfcd52af130dc
fsmflying <fangmingdev@163.com>
2018-08-13 21:47:43 +0800

    集群中添加主从配置,mysql-cluster-3307-slave,mysql-cluster-3308-slave,mysql-cluster-3309-slave

--- mysql-cluster-3307-slave/init/init.sql
+++ mysql-cluster-3307-slave/init/init.sql
@@ -1,44 +1,35 @@
-USE mysql;
+use mysql;
 GRANT ALL ON *.* TO 'root'@'%';
-FLUSH PRIVILEGES;
+flush privileges;
 ALTER USER 'root'@'localhost' IDENTIFIED BY 'fangming' PASSWORD EXPIRE NEVER;
 ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'fangming';
-FLUSH PRIVILEGES;
+flush privileges;
 
 
 CREATE DATABASE sequence;
 
 USE sequence;
 #
-CREATE TABLE MYCAT_SEQUENCE (
-  name          VARCHAR(50) NOT NULL,
-  current_value INT         NOT
-                            NULL,
-  increment     INT         NOT NULL DEFAULT 100,
-  PRIMARY KEY (name)
-)
-  ENGINE = InnoDB;
+CREATE TABLE MYCAT_SEQUENCE (name VARCHAR(50) NOT NULL,current_value INT NOT
+NULL,increment INT NOT NULL DEFAULT 100, PRIMARY KEY(name)) ENGINE=InnoDB;
 #插入一条 sequence
-INSERT INTO MYCAT_SEQUENCE (name, current_value, increment) VALUES ('GLOBAL', 100000, 100);
+INSERT INTO MYCAT_SEQUENCE(name,current_value,increment) VALUES ('GLOBAL', 100000,100);
 
 
 DROP FUNCTION IF EXISTS mycat_seq_currval;
 DELIMITER //
 CREATE FUNCTION mycat_seq_currval(seq_name VARCHAR(50))
-  RETURNS VARCHAR(64)
+  RETURNS varchar(64)
   CHARSET utf8mb4
 DETERMINISTIC
   BEGIN
     DECLARE retval VARCHAR(64);
-    SET retval = '-999999999,null';
-    SELECT concat(CAST(current_value AS CHAR), ',', CAST(increment AS CHAR))
-    INTO retval
-    FROM
-      MYCAT_SEQUENCE
-    WHERE name = seq_name;
+    SET retval='-999999999,null';
+    SELECT concat(CAST(current_value AS CHAR),',CAST(increment AS CHAR)) INTO retval FROM
+    MYCAT_SEQUENCE WHERE name = seq_name;
     RETURN retval;
-  END//
-DELIMITER ;
+  END
+    DELIMITER ;
 
 #设置 sequence 值
 DROP FUNCTION IF EXISTS mycat_seq_setval;
@@ -51,10 +42,11 @@ DETERMINISTIC
     SET current_value = value
     WHERE name = seq_name;
     RETURN mycat_seq_currval(seq_name);
-  END//
-DELIMITER ;
+  END
+    DELIMITER ;
 
-#取下一个 sequence 值
+
+#取下一个sequence值
 DROP FUNCTION IF EXISTS mycat_seq_nextval;
 DELIMITER //
 CREATE FUNCTION mycat_seq_nextval(seq_name VARCHAR(50))
@@ -68,16 +60,14 @@ DETERMINISTIC
   END//
 DELIMITER ;
 
-USE mix;
+use mix;
 
-CREATE TABLE sys_user
+create table sys_user
 (
-  id          BIGINT   AUTO_INCREMENT PRIMARY KEY,
-  username    VARCHAR(64) NOT NULL,
-  password    VARCHAR(64) NOT NULL,
-  mobile      VARCHAR(32) NOT NULL,
-  create_time DATETIME DEFAULT now(),
-  delete_time DATETIME    NULL
+  id bigint auto_increment primary key,
+  username varchar(64) not null,
+  password varchar(64) not null,
+  mobile varchar(32) not null,
+  create_time datetime default now(),
+  delete_time datetime null
 );
-
-
--- mysql-cluster-3309-slave/init/init.sql
+++ mysql-cluster-3309-slave/init/init.sql
@@ -4,5 +4,3 @@ flush privileges;
 ALTER USER 'root'@'localhost' IDENTIFIED BY 'fangming' PASSWORD EXPIRE NEVER;
 ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'fangming';
 flush privileges;
-
-
